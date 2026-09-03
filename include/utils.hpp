e72f973f61cd7b4d55d4f1a489a714e2313bcdd0
AlixRegnier <regnier.alix@outlook.com>
2026-09-03 16:16:38 +0200

    Removed unused functions (custom memory allocation/reallocation)

--- include/utils.hpp
+++ include/utils.hpp
@@ -13,31 +13,6 @@
 
 namespace block_compressor::utils
 {
-    template<typename T>
-    inline T* allocate(std::size_t new_size)
-    {
-        void* tmp = std::malloc(sizeof(T)*new_size);
-
-        if(!tmp)
-            throw block_compressor_error("utils", "malloc", "Malloc returned a null pointer, OOM ?");
-
-        return static_cast<T*>(tmp);
-    }
-
-    template<typename T>
-    inline T* reallocate(T* ptr, std::size_t old_size, std::size_t new_size)
-    {
-        if(old_size == new_size || ptr == nullptr)
-            return ptr;
-
-        void* tmp = std::realloc(ptr, sizeof(T)*new_size);
-
-        if(!tmp)
-            throw block_compressor_error("utils", "realloc", "Realloc returned a null pointer, OOM ?");
-
-        return static_cast<T*>(tmp);
-    }
-
     template <typename T>
     constexpr T ceil_div(T x, T y)
     {
